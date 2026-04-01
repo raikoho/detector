31c49e0dc63e72ffef4cdbe2c4296a09c813f18e
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 12:35:46 +0200

    Update module.h

--- module.h
+++ module.h
@@ -2,15 +2,16 @@
 #define MODULE_H
 
 #include <stdint.h>
+#include <sys/types.h>
 
 typedef struct {
     const char *name;
     void (*init)(void);
-    int (*check)(uint64_t prev_pc, uint64_t curr_pc);
+    int (*check)(uint64_t pid, uint64_t prev_pc, uint64_t curr_pc, uint64_t sp);
     void (*cleanup)(void);
 } detector_module_t;
 
-void register_module(detector_module_t mod);
-void run_modules(uint64_t prev_pc, uint64_t curr_pc);
+void register_module(detector_module_t m);
+void run_modules(uint64_t pid, uint64_t prev_pc, uint64_t curr_pc, uint64_t sp);
 
 #endif
