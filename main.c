36d06368fdcb0dd4e8ee1ca227febf935dc6805f
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 12:54:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,31 +4,81 @@
 #include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <sys/uio.h>
+#include <sys/types.h>
 #include <linux/ptrace.h>
 #include <elf.h>
-#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
 
 #include "module.h"
 
 extern detector_module_t cfi_module;
 
+/*
+ * ELF range of executable code (.text)
+ */
+uint64_t text_start = 0;
+uint64_t text_end   = 0;
+
 struct user_pt_regs regs;
 
 /*
- * Read PC + SP from traced process
+ * Parse ELF to find .text range
+ */
+void load_elf_range(const char *path) {
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("open ELF");
+        return;
+    }
+
+    Elf64_Ehdr eh;
+    read(fd, &eh, sizeof(eh));
+
+    lseek(fd, eh.e_shoff, SEEK_SET);
+
+    Elf64_Shdr sh;
+
+    for (int i = 0; i < eh.e_shnum; i++) {
+        read(fd, &sh, sizeof(sh));
+
+        if (sh.sh_flags & SHF_EXECINSTR) {
+            if (text_start == 0 || sh.sh_addr < text_start)
+                text_start = sh.sh_addr;
+
+            if (sh.sh_addr + sh.sh_size > text_end)
+                text_end = sh.sh_addr + sh.sh_size;
+        }
+    }
+
+    close(fd);
+
+    printf("[*] .text range: 0x%lx - 0x%lx\n", text_start, text_end);
+}
+
+/*
+ * Get registers
  */
 uint64_t get_regs(pid_t pid, uint64_t *sp_out) {
     struct iovec io;
     io.iov_base = &regs;
     io.iov_len = sizeof(regs);
 
-    if (ptrace(PTRACE_GETREGSET, pid, (void*)NT_PRSTATUS, &io) == -1) {
-        perror("ptrace(GETREGSET)");
-        return 0;
-    }
+    ptrace(PTRACE_GETREGSET, pid, (void*)NT_PRSTATUS, &io);
 
     *sp_out = regs.sp;
     return regs.pc;
+}
+
+/*
+ * REAL CFI CHECK
+ */
+void check_cfi(uint64_t pc) {
+    if (pc < text_start || pc > text_end) {
+        printf("\n[!!! CFI VIOLATION !!!]\n");
+        printf("Execution outside .text segment!\n");
+        printf("PC = 0x%lx\n", pc);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -38,29 +88,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    load_elf_range(argv[1]);
+
     pid_t pid = fork();
 
     if (pid == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execl(argv[1], argv[1], NULL);
-        perror("execl");
         return 1;
     }
 
     int status;
-    uint64_t prev_pc = 0;
     uint64_t sp = 0;
+    uint64_t pc;
 
-    /*
-     * Wait for child to stop at exec
-     */
     waitpid(pid, &status, 0);
 
-    register_module(cfi_module);
-
-    /*
-     * Start tracing
-     */
     ptrace(PTRACE_CONT, pid, NULL, NULL);
 
     while (1) {
@@ -68,34 +111,19 @@ int main(int argc, char *argv[]) {
         waitpid(pid, &status, 0);
 
         if (WIFEXITED(status)) {
-            printf("[*] Process exited normally (code=%d)\n", WEXITSTATUS(status));
-            break;
-        }
-
-        if (WIFSIGNALED(status)) {
-            printf("[!] Process killed by signal %d\n", WTERMSIG(status));
+            printf("[*] Process exited normally\n");
             break;
         }
 
         if (WIFSTOPPED(status)) {
 
-            uint64_t pc = get_regs(pid, &sp);
+            pc = get_regs(pid, &sp);
 
-            if (pc == 0) {
-                break;
-            }
+            check_cfi(pc);
 
-            run_modules(pid, prev_pc, pc, sp);
-
-            prev_pc = pc;
-
-            /*
-             * continue execution
-             */
             ptrace(PTRACE_SINGLESTEP, pid, NULL, NULL);
         }
     }
 
     return 0;
 }
-
