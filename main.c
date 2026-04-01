cf57f65edcfc79b09bb254f62fe99958202ea736
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 11:25:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include "module.h"
+#include <sys/user.h>
+#include <stdint.h>
+
+uint64_t get_pc(pid_t pid) {
+    struct user_regs_struct regs;
+    ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+    return regs.rip;
+}
 
 extern detector_module_t cfi_module;
 
