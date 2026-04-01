34496140b454f34085e727e293c3ebc9aa38fdba
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 12:35:59 +0200

    Update core.c

--- core.c
+++ core.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "module.h"
 
-#define MAX_MODULES 10
+static detector_module_t module;
 
-static detector_module_t modules[MAX_MODULES];
-static int module_count = 0;
-
-void register_module(detector_module_t mod) {
-    if (module_count < MAX_MODULES) {
-        modules[module_count++] = mod;
-        if (mod.init) mod.init();
-    }
+void register_module(detector_module_t m) {
+    module = m;
 }
 
-void run_modules(uint64_t prev_pc, uint64_t curr_pc) {
-    for (int i = 0; i < module_count; i++) {
-        if (modules[i].check(prev_pc, curr_pc)) {
-            printf("[ALERT] %s detected control-flow violation!\n",
-                   modules[i].name);
-        }
+void run_modules(uint64_t pid, uint64_t prev_pc, uint64_t curr_pc, uint64_t sp) {
+    if (module.check) {
+        module.check(pid, prev_pc, curr_pc, sp);
     }
 }
